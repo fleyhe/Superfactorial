97bdc8f7f8a185819521c17b90a1f13ba948a973
Marlon Henry Schweigert <fleyhe0@gmail.com>
2017-06-02 20:12:56 -0300

    Mongo find number

--- mongo/main.rb
+++ mongo/main.rb
@@ -9,12 +9,19 @@ def sfat n
 
 end
 
+def findFat n
+  return $client[:fat].find {:n => n.to_s}.to_a[0][:f]
+end
+
+def findSfat n
+  return $client[:sfat].find {:n => n.to_s}.to_a[0][:f]
+end
 
 require 'mongo'
 
 begin
   $client = Mongo::Client.new [ '127.0.0.1:27017' ], :database => 'superfactorial'
-  
+
 rescue => e
   puts e
   $client.close
