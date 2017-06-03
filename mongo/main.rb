e362315f7d3fda1edb899fa105479a05c67ab105
Marlon Henry Schweigert <fleyhe0@gmail.com>
2017-06-02 21:32:48 -0300

    Mongo Tests ok

--- mongo/main.rb
+++ mongo/main.rb
@@ -1,28 +1,65 @@
 #!/usr/bin/ruby
 # Stress test with superfactorial for MongoDB.
 
-def fat n
+# clear mongoDB
+system("mongo superfactorial < resetmongo")
 
-end
-
-def sfat n
-
-end
-
-def findFat n
-  return $client[:fat].find {:n => n.to_s}.to_a[0][:f]
-end
-
-def findSfat n
-  return $client[:sfat].find {:n => n.to_s}.to_a[0][:f]
-end
 
 require 'mongo'
 
-begin
-  $client = Mongo::Client.new [ '127.0.0.1:27017' ], :database => 'superfactorial'
+# Start the client
+Mongo::Logger.logger.level = ::Logger::FATAL
+$client = Mongo::Client.new [ '127.0.0.1:27017' ], :database => 'superfactorial'
 
-rescue => e
-  puts e
-  $client.close
+# Factorial function
+def fat n
+  puts("buscando fat(#{n})")
+  return 1 if n == 1 or n == 0
+  f = findFat(n)
+  if f == nil
+    f = fat(n-1)*n
+    insertFat n, f
+    return f
+  end
+  return f
 end
+
+# Superfactorial function
+def sfat n
+  puts("buscando sfat(#{n})")
+  return 1 if n == 1 or n == 0
+  f = findSfat(n)
+  if f == nil
+    f = sfat(n-1)*fat(n)
+    insertSfat n, f
+    return f
+  end
+  return f
+end
+
+def insertFat n, f
+  puts "Inserindo fat(#{n})"
+  doc = { :_id => BSON::ObjectId.new, :n => n.to_s, :f => f.to_s }
+  $client[:fat].insert_one doc
+end
+
+def insertSfat n, f
+  puts "Inserindo sfat(#{n})"
+  doc = { :_id => BSON::ObjectId.new, :n => n.to_s, :f => f.to_s }
+  $client[:sfat].insert_one doc
+end
+
+def findFat n
+  r = $client[:fat].find({:n => n.to_s}).to_a()[0]
+  return nil if r == nil
+  return r["f"].to_i
+end
+
+def findSfat n
+  r = $client[:sfat].find({:n => n.to_s}).to_a()[0]
+  return nil if r == nil
+  return r["f"].to_i
+end
+
+
+sfat(1000)
